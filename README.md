c48498708a13a980d0b7276a98908813f45f61f9
root <root@RyanDesktop.>
2025-05-18 13:13:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Development Environment Setup for WSL2 Ubuntu 24.04
+
+This repository should be cloned as root to /setup/
---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Setup for Ubuntu-24.04 WSL2 Development Environment
+## This script should be run as the desired user profile. Changes to root will be made automatically
+
+sudo apt update
